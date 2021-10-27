15a654e48cb15029e24381e3f34a9f0beb8d1d57
Manuel <skyppid@users.noreply.github.com>
2021-10-27 16:17:19 +0200

    Upgraded to AsyncPackage, fixed manifest

--- ArrayVisualizerExt/ArrayVisualizerExtPackage.cs
+++ ArrayVisualizerExt/ArrayVisualizerExtPackage.cs
@@ -3,35 +3,21 @@ using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
 namespace ArrayVisualizerExt
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    ///
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
-    /// register itself and its components with the shell.
-    /// </summary>
-    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-    // a package.
-    [PackageRegistration(UseManagedResourcesOnly = true)]
-    // This attribute is used to register the information needed to show the this package
-    // in the Help/About dialog of Visual Studio.
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("Array Visualizer",
-        "Visualizes 1D, 2D, 3D and 4D arrays and SharpDX Vectors and Matrices", "1.0", IconResourceID = 400)]
-    // This attribute is needed to let the shell know that this package exposes some menus.
+        "Visualizes 1D, 2D, 3D and 4D arrays and SharpDX Vectors and Matrices", "1.7", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    // This attribute registers a tool window exposed by this package.
     [ProvideToolWindow(typeof(ArrayVisualizerToolWindow))]
     [Guid(GuidList.GuidArrayVisualizerExtPkgString)]
-    public sealed class ArrayVisualizerExtPackage : Package
+    public sealed class ArrayVisualizerExtPackage : AsyncPackage
     {
         /// <summary>
         /// Default constructor of the package.
@@ -77,20 +63,23 @@ namespace ArrayVisualizerExt
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
-        protected override void Initialize()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}",
                 ToString()));
-            base.Initialize();
+            Initialize();
+
+            progress.Report(new ServiceProgressData("Registering menu commands for ArrayVisualizer."));
 
             // Add our command handlers for menu (commands must exist in the .vsct file)
-            ThreadHelper.ThrowIfNotOnUIThread();
-            if (GetService(typeof(IMenuCommandService)) is OleMenuCommandService mcs)
+            if (await GetServiceAsync(typeof(IMenuCommandService)) is OleMenuCommandService mcs)
             {
                 // Create the command for the tool window
                 CommandID toolwndCommandId = new CommandID(GuidList.GuidArrayVisualizerExtCmdSet,
                     (int) PkgCmdIdList.ArrayVisualizerTool);
                 MenuCommand menuToolWin = new MenuCommand(ShowToolWindow, toolwndCommandId);
+
+                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
                 mcs.AddCommand(menuToolWin);
                 //GlobalVars.menuToolWin = menuToolWin;
             }
